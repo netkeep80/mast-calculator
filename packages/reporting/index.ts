e573ee29fa3a2e53cc1d45d1bd4dfab066727456
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-08 10:37:57 +0300

    Migrate reporting public entrypoint to strict TypeScript

---
+++ packages/reporting/index.ts
@@ -0,0 +1,7 @@
+export * from './src/report.js'
+export * from './src/calculation-note.js'
+export * from './src/calculation-project.js'
+export * from './src/fabrication-project-appendix.js'
+export * from './src/eskd-construction-documentation.js'
+export * from './src/reference-data.js'
+export type * from './src/contracts.js'

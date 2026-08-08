eac7204796770fbe4e4daa4c8f0e40c77044dd1d
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-08-08 18:03:12 +0300

    Export canonical procurement application use case

--- packages/application/index.ts
+++ packages/application/index.ts
@@ -7,6 +7,7 @@ export * from './src/joint-preview.js'
 export * from './src/fabrication-preview.js'
 export * from './src/artifacts.js'
 export * from './src/result-summary.js'
+export * from './src/procurement.js'
 export * from './src/cancellation.js'
 export * from './src/errors.js'
 export {

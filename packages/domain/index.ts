38d9cf1351477ee100ead336f27d77f05286c187
Vertushkin Roman Pavlovich <netkeep80@gmail.com>
2026-09-14 20:42:09 +0300

    fix: expose load-action migration contract symbols

--- packages/domain/index.ts
+++ packages/domain/index.ts
@@ -10,3 +10,8 @@ export * from './src/wind-action.js'
 export * from './src/weld-service-degradation.js'
 export * from './src/project-parameters.js'
 export * from './src/project-package.js'
+export {
+  MANUAL_MIGRATED_V1_LOAD_ACTION_PROFILE,
+  PROJECT_PACKAGE_SCHEMA_V1,
+  SP20_OPERATIONAL_LOAD_ACTION_PROFILE,
+} from './src/contracts.js'
